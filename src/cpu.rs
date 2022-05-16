5087b14b1e1d9029f42f1252b725c58dff1a1f2b
Buchimi <stormzzy900@gmail.com>
2022-05-15 21:27:05 -0500

    Implement bit flags

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,29 @@
 use crate::opcodes;
 use std::collections::HashMap;
 
+bitflags! {
+    ///  7 6 5 4 3 2 1 0
+    ///  N V _ B D I Z C
+    ///  | |   | | | | +--- Carry Flag
+    ///  | |   | | | +----- Zero Flag
+    ///  | |   | | +------- Interrupt Disable
+    ///  | |   | +--------- Decimal Mode (not used on NES)
+    ///  | |   +----------- Break Command
+    ///  | +--------------- Overflow Flag
+    ///  +----------------- Negative Flag
+
+    pub struct CpuFlags : u8 {
+        const CARRY             = 0b00000001;
+        const ZERO              = 0b00000010;
+        const INTERRUPT_DISABLE = 0b00000100;
+        const DECIMAL_MODE      = 0b00001000;
+        const BREAK             = 0b00010000;
+        const BREAK2            = 0b00100000;
+        const OVERFLOW          = 0b01000000;
+        const NEGATIV           = 0b10000000;
+    }
+}
+
 pub struct CPU {
     pub register_a: u8,
     pub program_counter: u16,
